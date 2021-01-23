4dbe1b7541c67ba465315c4d285a93ab1759a740
Udit Kapoor <ukapoor.29@gmail.com>
2021-01-23 14:55:55 +0530

    graph

--- services/Reddit/app_test.py
+++ services/Reddit/app_test.py
@@ -14,15 +14,29 @@ firebase_app = fb_class.fire_base_app()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = pd.read_csv(r'bitcoin_historical_data.csv')
+    data = data.drop(['Open', 'High', 'Low', 'Vol.', 'Change %'], axis=1)
+    for col in data.columns:
+        print(col)
+    print(data) 
+    data.iloc[::-1]
+    legend = 'Monthly Data'
+    legend1 = "Test"
+    labels = list(data.Date)
+    values = list(data.Price)
+    values = [i.replace(',','') for i in values]
+    #values = [int(i) for i in values] 
+    print(values[0])
+    
+    return render_template('index.html', values=values, labels=labels, legend=legend, legend1=legend1)
 
 
 if __name__ == "__main__":
 
-    exchanges = api.metadata_list_exchanges()
+    #exchanges = api.metadata_list_exchanges()
 
-    for exchange in exchanges:
-        print(exchange['name'])
+    #for exchange in exchanges:
+    #    print(exchange['name'])
     app.run(debug=True)
 '''
     data_table = reddit.create_table(reddit.bitcoin)
--- services/Reddit/coinbase_test.py
+++ services/Reddit/coinbase_test.py
@@ -1,10 +1,11 @@
 from coinbase.wallet.client import Client
+import pandas as pd
 
 api_key = '9TqBvv4hpY4z2WTZ'
 api_secret = '7NAXuR71bJ5QPTRoVj3QgnqD2zaHrfke'
 client = Client(api_key, api_secret)
 
-
-price = client.get_spot_price(currency_pair = 'BTC-USD')#, date='2021-01-08')
+price = client.get_spot_price(currency_pair = 'BTC-USD')
 
 print(price)
+
